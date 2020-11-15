d2bece2ab841cb535d7ba79a37956b8a97d0af02
tnemelclement <clementmouly2509@gmail.com>
2020-11-15 15:39:06 +0100

    test

--- assets/classes/gonflables-class.js
+++ assets/classes/gonflables-class.js
@@ -163,7 +163,7 @@ let Gonflables = class {
                     if (result[0] != undefined)
                         next(result[0])
                     else
-                        next(new Error(config.errors.wrongID))
+                        next(new Error(config.errors.wrongID + "ahhhhh"))
                 })
                 .catch((err) => next(err))
         })
