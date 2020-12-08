b28a9f974c9b0e12cab895e41ca2d7b87db6955f
tnemelclement <clementmouly2509@gmail.com>
2020-12-08 15:04:38 +0100

    ajout categorie

--- assets/classes/gonflables-class.js
+++ assets/classes/gonflables-class.js
@@ -158,7 +158,7 @@ let Gonflables = class {
     // Récupère un jeu En Stock
     static getEnStockByID(id) {
         return new Promise((next) => {
-            db.query('SELECT * FROM jeuxEnStock WHERE id = ?', [id])
+            db.query('SELECT * FROM jeuxEnStock WHERE _ref = ?', [id])
                 .then((result) => {
                     if (result[0] != undefined)
                         next(result[0])
